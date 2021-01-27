a33ba81054b6f601f92d5c66652bb5dfbf41b1b2
cecyramallo <cecyramallo@gmail.com>
2021-01-27 11:22:38 -0400

    Template Login con p para los íconos y frase para Sign Up

--- src/view/1templateLogin.js
+++ src/view/1templateLogin.js
@@ -7,14 +7,15 @@ export const login = () => {
         </div>
       </div> 
       <containerLogin class="containerLogin">
-        <input type="text" class="input_name" name="name" id="name" placeholder="User Name" />
-        <input type="password" class="input_name" name="name" id="name" placeholder="Password" />
+        <input type="text" class="input_name" name="name" id="name" placeholder="User Name"/>
+        <input type="password" class="input_name" name="name" id="name" placeholder="Password"/>
         <button class="buttonLogin">Log in</button>
+        <p>Or log in with</p>
         <containerIcons class="containerIcons">
           <icons class="socialIcons"><img src="images/02_Google_a.png" onclick="ir_google()"/></icons>
           <icons class="socialIcons"><img src="images/01_Facebook.png" onclick="ir_face()"/></icons>
         </containerIcons>
-        <h2>Registrate</h2>
+        <p>Don't have an account yet? Sign up!</p>
       </containerLogin>
       `
     return viewLogin;
