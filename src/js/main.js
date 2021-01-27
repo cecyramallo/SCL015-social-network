7e5db48f27ecb5001660ef8b908c5470633f261d
Eileencisternas <eileencisternasr@gmail.com>
2021-01-27 12:48:04 -0300

    ignorar

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,5 @@ import { myFunction } from './index.js';
 import { login } from '../view/1templateLogin.js';
 
 myFunction();
-
+//Función login de página Bienvenida
 document.getElementById("root").innerHTML= login(); 
--- src/view/1templateLogin.js
+++ src/view/1templateLogin.js
@@ -1,12 +1,21 @@
 export const login = () => {
+<<<<<<< HEAD
     const viewLogin = ` 
       <h1 href="#/">“PARENTBOOK”</h1>
       <div class="container">
         <div class="box">
+=======
+    const viewLogin = 
+    ` 
+      <h1 href="#/">“PARENTBOOK”</h1>
+      <div class="container">
+      <div class="box">
+>>>>>>> 01f2ffb (ignorar)
           <img src="images/04_Family_a.jpg"/>
         </div>
       </div> 
       <containerLogin class="containerLogin">
+<<<<<<< HEAD
         <input type="text" class="input_name" name="name" id="name" placeholder="User Name"/>
         <input type="password" class="input_name" name="name" id="name" placeholder="Password"/>
         <button class="buttonLogin">Log in</button>
@@ -20,3 +29,34 @@ export const login = () => {
       `
     return viewLogin;
 };
+=======
+        <input type="text" class="input_name" name="name" id="name" placeholder="User Name" />
+        <input type="password" class="input_name" name="name" id="name" placeholder="Password" />
+        <button class="buttonLogin">Log in</button>
+        <containerIcons class="containerIcons">
+          <icons class="socialIcons"><img src="images/google1.png" onclick="ir_google()"  /></icons>
+          <icons class="socialIcons"><img src="images/facebook.png" onclick="ir_face()"/></icons>
+        </containerIcons>
+        <h2>Registrate</h2>
+      </containerLogin> 
+      `
+    return viewLogin;
+};
+
+
+ <div class="container">
+<div class="box">
+    <img src="images/04_Family_a.jpg"/>
+  </div>
+</div> 
+<containerLogin class="containerLogin">
+  <input type="text" class="input_name" name="name" id="name" placeholder="User Name" />
+  <input type="password" class="input_name" name="name" id="name" placeholder="Password" />
+  <button class="buttonLogin">Log in</button>
+  <containerIcons class="containerIcons">
+    <icons class="socialIcons"><img src="images/google1.png" onclick="ir_google()"  /></icons>
+    <icons class="socialIcons"><img src="images/facebook.png" onclick="ir_face()"/></icons>
+  </containerIcons>
+  <h2>Registrate</h2>
+</containerLogin> 
+>>>>>>> 01f2ffb (ignorar)
