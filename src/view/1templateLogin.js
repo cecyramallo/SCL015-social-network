1a3672dcadfef6121cafe5f2439b9a1cb3288cc6
Eileencisternas <eileencisternasr@gmail.com>
2021-01-27 12:41:09 -0300

    test

--- src/view/1templateLogin.js
+++ src/view/1templateLogin.js
@@ -22,18 +22,3 @@ export const login = () => {
 };
 
 
- <div class="container">
-<div class="box">
-    <img src="images/04_Family_a.jpg"/>
-  </div>
-</div> 
-<containerLogin class="containerLogin">
-  <input type="text" class="input_name" name="name" id="name" placeholder="User Name" />
-  <input type="password" class="input_name" name="name" id="name" placeholder="Password" />
-  <button class="buttonLogin">Log in</button>
-  <containerIcons class="containerIcons">
-    <icons class="socialIcons"><img src="images/google1.png" onclick="ir_google()"  /></icons>
-    <icons class="socialIcons"><img src="images/facebook.png" onclick="ir_face()"/></icons>
-  </containerIcons>
-  <h2>Registrate</h2>
-</containerLogin> 
